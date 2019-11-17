5836239340df9c8a2d4f13697150291bc63c6f46
Madina <madina.deryagina@yandex.ru>
2019-11-17 20:05:20 +0300

    with optional CommandLineRunner

--- src/main/java/otus/deryagina/spring/question/QuestionOnSpringBootApplication.java
+++ src/main/java/otus/deryagina/spring/question/QuestionOnSpringBootApplication.java
@@ -1,21 +1,29 @@
 package otus.deryagina.spring.question;
 
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import otus.deryagina.spring.question.interact.InteractService;
+
 
 @SpringBootApplication
 public class QuestionOnSpringBootApplication {
 
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(QuestionOnSpringBootApplication.class, args);
-        try {
-            context.getBean(InteractService.class).startInteraction();
-        }catch (NoSuchBeanDefinitionException ex){
-            System.exit(0);
-        }
+        SpringApplication.run(QuestionOnSpringBootApplication.class, args);
+    }
+
+    @Bean
+    @ConditionalOnBean(InteractService.class)
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            InteractService interactService=ctx.getBean(InteractService.class);
+            interactService.startInteraction();
+        };
     }
 
 }
